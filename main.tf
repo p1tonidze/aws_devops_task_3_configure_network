6baaf768a148898d805f8a6af0b80a60e437f49e
p1tonidze <p1t0nidze422@gmail.com>
2025-03-03 13:37:46 +0200

    Solution

--- main.tf
+++ main.tf
@@ -1,13 +1,74 @@
 # Add your code here: 
 
-# 1. Create a subnet 
+# 1. Create a subnet
+resource "aws_subnet" "subnet" {
+  vpc_id     = var.vpc_id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "grafana"
+  }
+}
+
 # 2. Create an Internet Gateway and attach it to the vpc
+resource "aws_internet_gateway" "gw" {
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = "mate-aws-grafana-lab"
+  }
+}
+
 # 3. Configure routing for the Internet Gateway
-# 4. Create a Security Group and inbound rules 
-# 5. Uncommend (and update the value of security_group_id if required) outbound rule - it required 
-# to allow outbound traffic from your virtual machine: 
-# resource "aws_vpc_security_group_egress_rule" "allow_all_eggress" {
-#   security_group_id = aws_security_group.security_group.id
-#   cidr_ipv4   = "0.0.0.0/0"
-#   ip_protocol = -1
-# }
+resource "aws_route_table" "route_table" {
+  vpc_id = var.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "mate-aws-grafana-lab"
+  }
+
+}
+
+resource "aws_route_table_association" "route_table_association" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+# 4. Create a Security Group and inbound rules
+resource "aws_security_group" "sg" {
+  name        = "mate-aws-grafana-lab"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "mate-aws-grafana-lab"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.sg.id
+
+  from_port   = 3000
+  to_port     = 3000
+  ip_protocol = "tcp"
+  cidr_ipv4   = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.sg.id
+
+  from_port   = 22
+  to_port     = 22
+  ip_protocol = "tcp"
+  cidr_ipv4   = "176.36.20.59/32"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_eggress" {
+  security_group_id = aws_security_group.sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = -1
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,1 @@
-vpc_id = ""
+vpc_id = "vpc-0390fdb5c2cfae8da"
